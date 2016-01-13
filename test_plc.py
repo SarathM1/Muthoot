917a0055e9d265133a6116f0f3f0fb046cb4abcd
Sarath M <sarathm00@gmail.com>
2016-01-13 14:53:24 +0530

    Code to test PLC

---
+++ test_plc.py
@@ -0,0 +1,41 @@
+import minimalmodbus
+import serial
+import time
+
+start_addr = 0
+end_addr = 9
+add = start_addr
+
+
+plc = minimalmodbus.Instrument('/dev/ttyS0',2)
+#plc.debug = True
+plc.serial.baudrate = 9600
+plc.serial.bytesize = 7
+plc.serial.parity = serial.PARITY_EVEN
+plc.serial.stopbits = 1
+plc.serial.timeout = 0.1
+plc.mode = minimalmodbus.MODE_ASCII
+while True:
+	try:
+		if add == (end_addr+1):
+			add = start_addr
+			print "\n",
+		
+		try:
+			value = plc.read_register(add,signed=True)
+			
+			plc.write_register(1, -100, signed=True)
+		except Exception, e:
+			print e,add
+
+		else:
+			print add,'=',value,'|',
+			#break
+		
+		#time.sleep(0.1)
+		add += 1
+	except serial.SerialException :
+		print('\nProgram aborted because there is error in opening the serial port.\n' )
+
+	   
+			
