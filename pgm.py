a21731aeefe0a78ee5008937be34e55f463cf0e6
Sarath M <sarathm00@gmail.com>
2016-01-13 13:33:29 +0530

    Working

--- pgm.py
+++ pgm.py
@@ -2,40 +2,40 @@ import minimalmodbus
 import serial
 import time
 
-add = 10
+add = 0
 
-if __name__ == '__main__':
-	while True:
+#if __name__ == '__main__':
+while True:
+	try:
+		if add == 2:
+			add = 0
+			print "\n",
+		
+		plc = minimalmodbus.Instrument('/dev/ttyS0',2)
+		#plc.debug = True
+		plc.serial.baudrate = 9600
+		plc.serial.bytesize = 7
+		plc.serial.parity = serial.PARITY_EVEN
+		plc.serial.stopbits = 1
+		plc.serial.timeout = 0.1
+		plc.mode = minimalmodbus.MODE_ASCII
+
+		
 		try:
-			if add == 14:
-				add = 10
-				print "\n",
+			value = plc.read_register(add,signed=True)
 			
-			instrument = minimalmodbus.Instrument('/dev/ttyS0',2)
-			#instrument.debug = True
-			instrument.serial.baudrate = 9600
-			instrument.serial.bytesize = 7
-			instrument.serial.parity = serial.PARITY_EVEN
-			instrument.serial.stopbits = 1
-			instrument.serial.timeout = 0.1
-			instrument.mode = minimalmodbus.MODE_ASCII
+			plc.write_register(1, -100, signed=True)
+		except Exception, e:
+			print e,add
 
-			
-			try:
-				#value2 = instrument.read_long(add)
-				value = instrument.read_register(add) 
-				#value3 = instrument.read_bit(add)
-			except Exception, e:
-				print repr(e),add
-
-			else:
-				print value,
-				#break
-			
-			time.sleep(0.1)
-			add += 1
-		except serial.SerialException :
-			print('\nProgram aborted because there is error in opening the serial port.\n' )
+		else:
+			print add,'=',value,'|',
+			#break
+		
+		#time.sleep(0.1)
+		add += 1
+	except serial.SerialException :
+		print('\nProgram aborted because there is error in opening the serial port.\n' )
 
 	   
 			
