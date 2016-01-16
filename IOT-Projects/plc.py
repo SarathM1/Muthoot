20b73c24a7d2a4f6f12055a11789bbf227b3ab66
Sarath M <sarathm00@gmail.com>
2016-01-16 13:39:30 +0530

    Added all register's

--- IOT-Projects/plc.py
+++ IOT-Projects/plc.py
@@ -15,13 +15,64 @@ class PLC(object):
 		self.plc.serial.stopbits = 1
 		self.plc.serial.timeout = 0.1
 		self.plc.mode = minimalmodbus.MODE_ASCII
-		
+	
+	def dummyPacket():
+		mode = ['Auto','Manual']
+		status=['Off','On']
+		val = {}
+
+		val['time']				= time.strftime('%Y-%m-%d %H:%M:%S',time.localtime())
+		val['mode']				= 0
+		val['mains_status']   	= 0
+		val['mains_voltage']	= 0
+		val['mains_load']		= 0
+		val['breaker1_status']	= 0
+		val['dg1_status']		= 0
+		val['dg1_alarm']		= 0
+		val['dg1_voltage']		= 0
+		val['dg1_load']			= 0
+		val['dg1_hours']		= 0
+		val['breaker2_status']	= 0
+		val['bus_coupler_status'] = 0
+		val['dg2_status']		= 0
+		val['dg2_alarm']		= 0
+		val['dg2_voltage']		= 0
+		val['dg2_load']			= 0
+		val['dg2_hours']		= 0
+		val['breaker3_status']	= 0
+
+
+		return val
+
 	def readRegister(self,addr):
+		mode = ['Auto','Manual']
+		status=['Off','On']
+
 		try:
-			value = self.plc.read_register(addr,signed=True)
+			val['time']				= time.strftime('%Y-%m-%d %H:%M:%S',time.localtime())
+			val['mode']				= self.plc.read_register(0)
+			val['mains_status']   	= self.plc.read_register(1)
+			val['mains_voltage']	= self.plc.read_register(2)
+			val['mains_load']		= self.plc.read_register(3)
+			val['breaker1_status']	= self.plc.read_register(4)
+			val['dg1_status']		= self.plc.read_register(5)
+			val['dg1_alarm']		= self.plc.read_register(6)
+			val['dg1_voltage']		= self.plc.read_register(7)
+			val['dg1_load']			= self.plc.read_register(8)
+			val['dg1_hours']		= self.plc.read_register(9)
+			val['breaker2_status']	= self.plc.read_register(10)
+			val['bus_coupler_status'] = self.plc.read_register(11)
+			val['dg2_status']		= self.plc.read_register(12)
+			val['dg2_alarm']		= self.plc.read_register(13)
+			val['dg2_voltage']		= self.plc.read_register(14)
+			val['dg2_load']			= self.plc.read_register(15)
+			val['dg2_hours']		= self.plc.read_register(16)
+			val['breaker3_status']	= self.plc.read_register(17)
 		except Exception, e:
-			print "plc_read: ",e
+			val = dummyPacket()
+			print "readRegister: ",e
 		
-		return value
+		return val
 
 plc = PLC()
+
