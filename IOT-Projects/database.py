34b3afb3cfe5b7261ca5c8ce61414028c1dfa00f
Work PC <sarathm00@gmail.com>
2016-01-18 12:52:45 +0530

    Working

--- IOT-Projects/database.py
+++ IOT-Projects/database.py
@@ -8,59 +8,78 @@ class db():
 		self.obj=pymysql.connect(host=db_host, user=db_user, passwd=db_passwd,db=self.db_name)
 		self.cur=self.obj.cursor()
 		try:
-			table_create()
+			self.table_create()
 		except Exception as e:
 			print e
 			pass	
 		self.cur.close()
 	
-	def table_create():
-	    cur.execute('CREATE TABLE db (id INTEGER NOT NULL AUTO_INCREMENT,\
-	     time DATETIME,\
-	     mode VARCHAR(8),\
-	     mains_status VARCHAR(8),\
-	     mains_voltage VARCHAR(8),\
-	     mains_load VARCHAR(8),\
-	     breaker1_status VARCHAR(8),\
-	     dg1_status VARCHAR(8),\
-	     dg1_alarm VARCHAR(8),\
-	     dg1_voltage VARCHAR(8),\
-	     dg1_load VARCHAR(8),\
-	     dg1_hours VARCHAR(8),\
-	     breaker2_status VARCHAR(8),\
-	     bus_coupler_status VARCHAR(8),\
-	     dg2_status VARCHAR(8),\
-	     dg2_alarm VARCHAR(8),\
-	     dg2_voltage VARCHAR(8),\
-	     dg2_load VARCHAR(8),\
-	     dg2_hours VARCHAR(8),\
-	     breaker3_status VARCHAR(8),\
-	     PRIMARY KEY (id))')	
+	def table_create(self):
+		self.cur.execute('CREATE TABLE tb (id INTEGER NOT NULL AUTO_INCREMENT,\
+		 time DATETIME,\
+		 mode VARCHAR(8),\
+		 mains_status VARCHAR(8),\
+		 mains_voltage VARCHAR(8),\
+		 mains_load VARCHAR(8),\
+		 breaker1_status VARCHAR(8),\
+		 dg1_status VARCHAR(8),\
+		 dg1_alarm VARCHAR(8),\
+		 dg1_voltage VARCHAR(8),\
+		 dg1_load VARCHAR(8),\
+		 dg1_hours VARCHAR(8),\
+		 breaker2_status VARCHAR(8),\
+		 bus_coupler_status VARCHAR(8),\
+		 dg2_status VARCHAR(8),\
+		 dg2_alarm VARCHAR(8),\
+		 dg2_voltage VARCHAR(8),\
+		 dg2_load VARCHAR(8),\
+		 dg2_hours VARCHAR(8),\
+		 breaker3_status VARCHAR(8),\
+		 PRIMARY KEY (id))')	
 
-	def insert(self,status):
+	def insert(self,val):
 		self.obj=pymysql.connect(host=db_host, user=db_user, passwd=db_passwd,db=self.db_name)
 		self.cur=self.obj.cursor()
 
 		time=datetime.strftime(datetime.now(), "%d/%m/%Y %H:%M:%S")
 		self.cur.execute('''INSERT INTO tb(time,\
-	     mode,\
-	     mains_status,\
-	     mains_voltage,\
-	     mains_load,\
-	     breaker1_status,\
-	     dg1_status,\
-	     dg1_alarm,\
-	     dg1_voltage,\
-	     dg1_load,\
-	     dg1_hours,\
-	     breaker2_status,\
-	     bus_coupler_status,\
-	     dg2_status,\
-	     dg2_alarm,\
-	     dg2_voltage,\
-	     dg2_load,\
-	     dg2_hours,\
-	     breaker3_status) VALUES(%s,%s)''',(time,status))
+		 mode,\
+		 mains_status,\
+		 mains_voltage,\
+		 mains_load,\
+		 breaker1_status,\
+		 dg1_status,\
+		 dg1_alarm,\
+		 dg1_voltage,\
+		 dg1_load,\
+		 dg1_hours,\
+		 breaker2_status,\
+		 bus_coupler_status,\
+		 dg2_status,\
+		 dg2_alarm,\
+		 dg2_voltage,\
+		 dg2_load,\
+		 dg2_hours,\
+		 breaker3_status) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)''',
+		(val['time'],
+			val['mode'],
+			val['mains_status'],
+			val['mains_voltage'],
+			val['mains_load'],
+			val['breaker1_status'],
+			val['dg1_status'],
+			val['dg1_alarm'],
+			val['dg1_voltage'],
+			val['dg1_load'],
+			val['dg1_hours'],
+			val['breaker2_status'],
+			val['bus_coupler_status'],
+			val['dg2_status'],
+			val['dg2_alarm'],
+			val['dg2_voltage'],
+			val['dg2_load'],
+			val['dg2_hours'],
+			val['breaker3_status']))
 		self.obj.commit() 
 		self.cur.close()
 
@@ -79,18 +98,18 @@ class db():
 		return res
 
 	def parseData(self,data):
-	    """ 
-	    This function splits the string into a tuple containing device, level, datetime 
-	    the function parameter should be a string 
-	    return type is a (devicename,level,datetime)
-	    """
-	    try:
-	        data = data.strip()     # It removes all the newline character from the string
-	        data = data.split(';')  # Splits the string at every ';' character
+		""" 
+		This function splits the string into a tuple containing device, level, datetime 
+		the function parameter should be a string 
+		return type is a (devicename,level,datetime)
+		"""
+		try:
+			data = data.strip()     # It removes all the newline character from the string
+			data = data.split(';')  # Splits the string at every ';' character
 
-	        val = {}
-	        val['time']				= datetime.strptime( data[0], "%Y-%m-%d %H:%M:%S")
-	        val['mode']				= data[1]
+			val = {}
+			val['time']				= datetime.strptime( data[0], "%Y-%m-%d %H:%M:%S")
+			val['mode']				= data[1]
 			val['mains_status']   	= data[2]
 			val['mains_voltage']	= data[3]
 			val['mains_load']		= data[4]
@@ -108,5 +127,7 @@ class db():
 			val['dg2_load']			= data[16]
 			val['dg2_hours']		= data[17]
 			val['breaker3_status']	= data[18]
+		except Exception as e:
+			print 'parseData:',e
 
 		return val
